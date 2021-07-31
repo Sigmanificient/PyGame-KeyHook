018e0fd15229aca1cd2b45becb4b4d45222fe984
Yohann Boniface <edhyjox@gmail.com>
2021-07-31 22:31:53 +0200

    drawing first bubble !

--- app/core.py
+++ app/core.py
@@ -23,15 +23,50 @@ class AppCore:
             )
         }
 
+        self.bubbles = ["App Started"]
+        self.font = pygame.font.SysFont("Consolas", 32)
+        self.refresh = pygame.USEREVENT + 1
         self.is_running = False
 
     def handle_event(self, event):
         if event.type == pygame.QUIT:
             self.is_running = False
+            return
+
+        if event.type == self.refresh:
+            self.draw()
+
+    def draw(self):
+        if not len(self.bubbles):
+            return
+
+        for word in self.bubbles:
+            text = self.font.render(word, True, ALPHA)
+            text_rect = text.get_rect()
+
+            text_rect.right = self.screen.get_width() - 20
+            text_rect.bottom = self.screen.get_height() - 60
+
+            pygame.draw.rect(
+                self.screen,
+                WHITE,
+                pygame.Rect(
+                    *(
+                        (x - 16) if c < 2 else (x + 32)
+                        for c, x in enumerate(list(text_rect))
+                    )
+                ),
+                border_radius=5
+            )
+
+            self.screen.blit(text, text_rect)
+
+            pygame.display.update()
 
     def run(self):
+        pygame.time.set_timer(self.refresh, 500)
         self.is_running = True
 
         while self.is_running:
             self.handle_event(pygame.event.wait())
-            pygame.display.update()
+            print("r")
